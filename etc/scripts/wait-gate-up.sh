388dcab87159bdba37bde233be5b35dda5c863a6
root <root@vm-hercules-sp.dev.kontur.ru>
2020-09-02 12:37:33 +0500

    add wait scripts

---
+++ etc/scripts/wait-gate-up.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+ATTEMPTS_MAX_COUNT=40
+TIMEOUT=10
+
+ATTEMPT=1
+while [ "$ATTEMPT" -le "$ATTEMPTS_MAX_COUNT" ]; do
+  if nc -z gate 6306; then
+      break;
+  fi;
+  echo "$ATTEMPT attempt failed."
+
+  if [ "$ATTEMPT" -eq "$ATTEMPTS_MAX_COUNT" ]; then
+      echo "Can't ping gate!"
+      exit 1;
+  fi;
+
+  ATTEMPT=$(($ATTEMPT+1))
+  sleep $TIMEOUT;
+done
