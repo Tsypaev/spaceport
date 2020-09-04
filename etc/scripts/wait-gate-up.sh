9c73e74171e015681531b9f3b4feb66a928b932d
tsypaev <root@vm-hercules-sp.dev.kontur.ru>
2020-09-04 11:41:30 +0500

    test

--- etc/scripts/wait-gate-up.sh
+++ etc/scripts/wait-gate-up.sh
@@ -23,10 +23,10 @@ docker run --network=host -v "/$(pwd)/etc/properties/gateway-client/metrics:/etc
 docker run --network=host -v "/$(pwd)/etc/properties/gateway-client/traces:/etc/hercules" vstk/hercules-gateway-client:0.38.0-SNAPSHOT
 
 ATTEMPTS_MAX_COUNT=5
-TIMEOUT=10
+TIMEOUT=20
 ATTEMPT=1
 while [ "$ATTEMPT" -le "$ATTEMPTS_MAX_COUNT" ]; do
-  if [ "$(curl localhost:9200/test-index/_count | jq .count)" = "1" ]; then
+  if [ "$(curl localhost:9200/test-index*/_count | jq .count)" = "1" ]; then
       echo "Succes find log in Elasticsearch"
       break;
   fi;
@@ -45,7 +45,7 @@ ATTEMPTS_MAX_COUNT=5
 TIMEOUT=10
 ATTEMPT=1
 while [ "$ATTEMPT" -le "$ATTEMPTS_MAX_COUNT" ]; do
-  if [ "$(curl localhost:6304/metrics/find?query=TestValue | jq .[0].leaf)" = "1" ]; then
+  if [ "$(curl localhost:6304/metrics/find?query=TestValue* | jq .[0].leaf)" = "1" ]; then
       echo "Succes find metric in Graphite"
       break;
   fi;
